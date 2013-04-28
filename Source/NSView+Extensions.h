df799c1babede0c1451574769f44f768620e42fc
Elmar Tampe <elmo@mountaincoders.ch>
2013-04-28 18:44:31 +0200

    Fixed comment

--- Source/NSView+Extensions.h
+++ Source/NSView+Extensions.h
@@ -10,13 +10,13 @@
 /** Sets the origin.x property of the view to left. */
 @property (nonatomic, assign) CGFloat left;
 
-/** Sets the origin.x property of the view to 'right - self.frame.size.width' */
+/** Sets the origin.x property of the view to 'right - self.frame.size.width'. */
 @property (nonatomic, assign) CGFloat right;
 
-/** Sets the origin.y property of the view to 'top - self.frame.size.height' */
+/** Sets the origin.y property of the view to 'top - self.frame.size.height'. */
 @property (nonatomic, assign) CGFloat top;
 
-/** Sets the origin.y property of the view to 'bottom' */
+/** Sets the origin.y property of the view to 'bottom'. */
 @property (nonatomic, assign) CGFloat bottom;
 
 /** Sets the size.width property of the view to width. */
@@ -25,7 +25,7 @@
 /** Sets the size.height property of the view to height. */
 @property (nonatomic, assign) CGFloat height;
 
-/** Sets the size.height property of the view to height. */
+/** Sets the size property of the view to 'size'. */
 @property (nonatomic, assign) CGSize size;
 
 @end
